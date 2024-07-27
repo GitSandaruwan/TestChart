6b2cee3b4f1752370c477c8587a372e4539372e5
unknown <isuru246sandaruwan@gmail.com>
2024-07-27 09:19:19 +0530

    Initial commit

---
+++ CarSystem/Default.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CarSystem._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <table>
+        <tr>
+            <td>
+                <asp:GridView ID="gvLineChart" runat="server" OnSelectedIndexChanged="gvLineChart_SelectedIndexChanged">
+                </asp:GridView>
+            </td>
+            <td>
+                <div id="MyLineChart"></div>
+            </td>
+        </tr>
+    </table>
+
+ 
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script>
+        $(document).ready(function () {
+            var lineData = <%=lineData%>;
+
+            $('#MyLineChart').highcharts({
+                chart: {
+                    type: 'bar'  /*spline,bar,pie*/
+                },
+                title: {
+                    text: 'Monthly High Temp Counts'
+                },
+                xAxis: {
+                    title: {
+                        text: 'Month'
+                    }
+                },
+                yAxis: {
+                    title: {
+                        text: 'Count'
+                    }
+                },
+                series: [{
+                    type: 'bar',
+                    name: 'Monthly High Temp',
+                    data: lineData
+                }]
+            });
+        });
+
+    </script>
+</asp:Content>
+
+
---
+++ CarSystem/Default.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+using System.Web.UI;
+
+namespace CarSystem
+{
+    public partial class _Default : Page
+    {
+        private SqlConnection con;
+        public string lineData;
+
+        public void Connection()
+        {
+
+            string sqlconnection = WebConfigurationManager.ConnectionStrings["myconnection"].ToString();
+            con = new SqlConnection(sqlconnection);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            LoadDataToChart();
+        }
+
+        public void LoadDataToChart()
+        {
+
+            Connection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT MonthValue, TotalMonthCount FROM [Wether] where EventType='HighTem'", con);
+
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+            dt.Load(cmd.ExecuteReader());
+
+            // Display details in the GridView
+            gvLineChart.DataSource = dt;
+            gvLineChart.DataBind();
+
+            lineData = "[";
+            foreach (DataRow dr in dt.Rows)
+            {
+
+                lineData += "[" + dr["MonthValue"] + "," + dr["TotalMonthCount"] + "],";
+            }
+
+            if (lineData.Length > 1)
+            {
+
+                lineData = lineData.Remove(lineData.Length - 1);
+            }
+            lineData += "]";
+            con.Close();
+        }
+
+        protected void gvLineChart_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
